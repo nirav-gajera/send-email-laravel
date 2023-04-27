310b0b076416a36ebdd8958fbe007201799838d2
nirav090801~ <2104070100009@silveroakuni.ac.in>
2023-04-27 10:14:53 +0530

    first commit

---
+++ app/Http/Controllers/SendEmailController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendMail;
+
+class SendEmailController extends Controller
+{
+    function index()
+    {
+     return view('send_email');
+    }
+
+    function send(Request $request)
+    {
+     $this->validate($request, [
+      'name'     =>  'required',
+      'email'  =>  'required|email',
+      'message' =>  'required'
+     ]);
+
+        $data = array(
+            'name'      =>  $request->name,
+            'message'   =>   $request->message
+        );
+
+     Mail::to('nirav.demo@gmail.com')->send(new SendMail($data)); 
+     return back()->with('success', 'Thanks for contact us!');
+
+    }
+}
+
+?>
---
+++ app/Mail/SendMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $data;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('nirav.admin@gmail.com')->subject('Laravel E-mail Demo')->view('dynamic_email_template')->with('data', $this->data);
+    }
+}
+
+?>
